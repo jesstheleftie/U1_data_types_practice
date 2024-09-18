6d1e2a490806ea5440d879892c0712aa3144be17
jesstheleftie <jessetang57@hotmail.com>
2024-09-18 13:18:08 -0700

    complete and submitting in

--- script.js
+++ script.js
@@ -4,13 +4,19 @@
  * Declare a variable called myFavoriteSong on one line and console.log the value
  * Then, using your Literals, console.log "my favorite song is..." with the song's value
  */
-
+let myFavouriteSong = "Die with a smile";
+console.log(`my favourite song is ${myFavouriteSong}`);
 /*
  * Prompt 2:
  *
  * Create 3 variables using let and const, then console.log each of them, using a string literal (` ${} `} in the log
  */
-
+let userName = "Jessica";
+console.log(`My name is ${userName}.`);
+let userAge = 31;
+console.log(`My age is ${userAge}.`);
+const userLocation = "Vancouver";
+console.log(`I live in ${userLocation}.`);
 /*
  * Prompt 3:
  *
@@ -21,7 +27,13 @@
  *   - divide your variable by 7
  *   - calculate the remainder of dividing your variable by 2
  */
+let secretNum = 21;
 
+console.log(secretNum + 2);
+console.log(secretNum - 2);
+console.log(secretNum * 3);
+console.log(secretNum / 7);
+console.log(secretNum % 2);
 /*
  * Prompt 4:
  *
@@ -31,55 +43,78 @@
  *
  * Create a variable called fullName and use addition to combine your firstName
  * and lastName variables.
- * Run a console.log that says 'hello my name is..." with your fullName. 
+ * Run a console.log that says 'hello my name is..." with your fullName.
  */
+const firstName = "Jessica";
+const lastName = "Tang";
+let fullName = firstName + " " + lastName;
 
+console.log(`hello my name is ${fullName}.`);
 
 /* Arrays */
 
 // Create an array of Movies, then log the 2nd movie in your array
 // Use your array methods to add a movie in to the Front of the array, then the Back of the Array, then to the 2nd index point of the array
 
-
+const movies = ["Star Wars", "Lala Land", "Lion King", "Frozen"];
+console.log(movies[1]);
+movies.unshift("Mulan");
+console.log(movies);
+movies.push("Harry Potter");
+console.log(movies);
+movies.splice(2, 0, "Twilight");
+console.log(movies);
 /*
  *
  * Retrieve "Marty McFly" from each of the arrays below.
  */
 
-let array1 = ['Marty Marion', 'Marty Feldman', 'Marty McFly', 'Marty Marion'];
+let array1 = ["Marty Marion", "Marty Feldman", "Marty McFly", "Marty Marion"];
+console.log(array1[2]);
 let array2 = [
-  ['Marty Feldman', 'Marty Marion'],
-  ['Marty Stuart', 'Marty McFly'],
-  ['Marty Jannetty', 'Marty Robbins']
+  ["Marty Feldman", "Marty Marion"],
+  ["Marty Stuart", "Marty McFly"],
+  ["Marty Jannetty", "Marty Robbins"],
 ];
+console.log(array2[1][1]);
 let array3 = [
-  ['Marty Feldman', ['Marty Marion']],
-  ['Marty Stuart', ['Marty Janetty', ['Marty McFly'], 'Marty Robbins']]
+  ["Marty Feldman", ["Marty Marion"]],
+  ["Marty Stuart", ["Marty Janetty", ["Marty McFly"], "Marty Robbins"]],
 ];
+console.log(array3[1][1][1][0]);
 
 /*
  *
  * Use the length of the array below to retrieve the second to last item.
  */
 
-let array4 = ['a', 'b', 'c', 'd', 'e'];
-
+let array4 = ["a", "b", "c", "d", "e"];
+const secondToLast = array4[array4.length - 2];
+console.log(secondToLast);
 /*
  *
  * Use the following arrays to answer the subprompts below.
  */
 
-let thom = ['Thom', 1000, 'Christchurch'];
-let karolin = ['Karolin', 16, 'New York'];
-let kristyn = ['Kristyn', 5, 'Pittsburgh'];
-let cathleen = ['Cathleen', 186, 'New York'];
+let thom = ["Thom", 1000, "Christchurch"];
+let karolin = ["Karolin", 16, "New York"];
+let kristyn = ["Kristyn", 5, "Pittsburgh"];
+let cathleen = ["Cathleen", 186, "New York"];
 
 // Cathleen decides that Thom can't be named "Thom" anymore. Remove "Thom" from
 // the thom array and replace it with "Tom".
-
+thom.splice(0, 1, "Tom");
+console.log(thom);
 // Karolin just had her birthday; change Karolin's array to reflect her being
 // a year older.
-
+karolin.splice(1, 1, 17);
+console.log(karolin);
 // Change Cathleen's hometown from New York to "Gotham City".
-
+cathleen.splice(2, 1, "Gotham City");
+console.log(cathleen);
 // Remove "Pittsburgh" from Kristyn's array and add "Oakland".
+kristyn.splice(2, 1, "Oakland");
+
+
+
+
